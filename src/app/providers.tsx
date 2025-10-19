bb3422cb1a23a3529bb96a4eaaac8a1e7f4fff91
xjaylandero <xjaylandero@gmail.com>
2025-10-19 15:00:07 +0800

    expense list

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import QueryProvider from "../components/Provider/QueryProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <QueryProvider>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryProvider>
+  );
 }
--- src/components/homepage/ExpenseList.tsx
+++ src/components/homepage/ExpenseList.tsx
@@ -1,3 +1,12 @@
+import { useExpenses } from "@/hooks/useExpenses";
+
 export function ExpenseList() {
-  return <>ExpenseList</>;
+  const { data: expenses, isLoading, error } = useExpenses("1");
+  if (isLoading) return <>Loading...</>;
+  if (error) return <>Error: {error.message}</>;
+  return (
+    <ul>
+      <h1>Perfect</h1>
+    </ul>
+  );
 }
