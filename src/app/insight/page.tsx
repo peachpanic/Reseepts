9ff1eafe2f0bba12d6d11d44e095767fd87bd96d
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2025-10-19 17:18:33 +0800

    working on insight page

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -4,9 +4,7 @@ import { TopSpendItem } from '@/components/insight/TopSpendItem';
 import { TopSpendList } from '@/components/insight/TopSpendList';
 
 export default function InsightPage() {
-  const data = [
-    {'test'}
-  ]; // Dummy data for the AreaChart
+
   return (
     <>
       <div>
@@ -16,20 +14,9 @@ export default function InsightPage() {
       <div className="bg-red-200 flex flex-others items-center justify-center">
         Statistics
       </div>
-      
-      <div>
-        <AreaChart style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-      responsive
-      data={data}
-      margin={{
-        top: 20,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }}></AreaChart>
-      </div>
 
       <TopSpendList />
 
     </>
   );
+}
